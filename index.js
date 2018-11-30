529249bc565b17126fc3a39dc82fa31ef06e22b1
Shu Chao /Daniel Kuang <danielkuang7@gmail.com>
2018-11-30 20:30:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function takeANumber(katzDeli, otherDeli){
+  if (katzDeli.length > 0 );
+  katzDeli.push(otherDeli); //['Ada']
+
+  return `Welcome, ${otherDeli}. You are number ${katzDeli.length} in line.`
+}
